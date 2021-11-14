b87db4eec914b953545e4fc47016e49ebd75c65f
Javlon <59303287+javlona@users.noreply.github.com>
2021-11-14 17:26:25 -0600

    Update and rename words.json to words.js

--- words.js
+++ words.js
@@ -1,4 +1,5 @@
-[
+var word =
+    [
    "abandon", "ability", "able", "abortion", "about", "above", "abroad", "absence", "absolute", "absolutely", "absorb", "abuse", "academic", "accept", "access",
    "accident", "accompany", "accomplish", "according", "account", "accurate", "accuse", "achieve", "achievement", "acid", "acknowledge", "acquire", "across", "act", 
    "action", "active", "activist", "activity", "actor","actress", "actual", "actually", "ad", "adapt", "add", "addition", "additional", "address", "adequate", "adjust", 
